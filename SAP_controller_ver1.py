3033148d2afd5ee486067ae330c0bb7e74611a20
geri2204 <115074676+geri2204@users.noreply.github.com>
2024-07-23 14:52:04 +0200

    Add files via upload

--- SAP_controller_ver1.py
+++ SAP_controller_ver1.py
@@ -1,158 +1,162 @@
-from pywinauto import Desktop
-from pywinauto import Application
+from pywinauto import Desktop, Application
 import time
 from pywinauto.keyboard import send_keys
-from pywinauto.mouse import click
 
 def GetWindow(name):
     for win in Desktop(backend="win32").windows():
         if name in win.window_text() and win.window_text() != "":
             return win
+    return None
         
-def SendCmd(n, cmd, delay = 0.2):
-    for i in range(0,n):
+def SendCmd(n, cmd, delay=0.2):
+    for i in range(n):
         send_keys(cmd)
         time.sleep(delay)
+
+def identify_controls(window):
+    if window:
+        try:
+            window.print_control_identifiers()
+        except AttributeError as e:
+            print(f"Error identifying: {e}")
+    else:
+        print("Nincs mit identify-olni")
 
 def Run(ertek):
     target_title = "EMP(1)/102 Bekötés megjelenítése: kezdő kép"
     
     # Megtalál és a célablakra fókuszál
     window = GetWindow(target_title)
+    identify_controls(window)
 
     if window:
-        print(f"Found target window: {window.window_text()}")
+        print(f"Célablak megtalálva: {window.window_text()}")
         window.set_focus()
-        time.sleep(2)  # vár
+        time.sleep(2)  # Várakozás
 
         # Ctrl+f
-        send_keys('^f')  # '^' Ctrl
-
-        print("Ctrl+F key combination sent.")
+        send_keys('^f')
+        print("Ctrl+F kombináció leütve")
     else:
-        print("Target window not found.")
+        print("Célablak nem található")
+        return
 
     print("Folyamat befejezve")
 
     target_title = "EMP(1)/102 Data Finder(adatkereső): Közműbekötés keresése"
-    # Megtalál és a célablakra fókuszál
     window = GetWindow(target_title)
 
     if window:
         print(f"Célablak megtalálva: {window.window_text()}")
         window.set_focus()
-        time.sleep(2)  # vár
+        time.sleep(2)  # Várakozás
 
-        
         send_keys('{TAB}')
         time.sleep(1)
-        SendCmd(2,'{RIGHT}')
+        SendCmd(2, '{RIGHT}')
         send_keys('{SPACE}')
         print("'Más kereső kritérium' - ablakra kattint! Vár!")
         time.sleep(2)
         send_keys('{TAB}')
         print("'Globális korlátozások' - alcímre lép!")
 
-        SendCmd(9,  '{DOWN}')
+        SendCmd(9, '{DOWN}')
         print("'Mér.pont megn.' - mezőt megkeresi!")
 
         send_keys(ertek)
         print("'POD-érték' - BEMÁSOLVA!")
 
         send_keys('{ENTER}')
-
-
-
-
-
-        #Várakozás a következő ablak megjelenésére
-        time.sleep(2)
-
-
-
-
-    # Megtalál és a célablakra fókuszál
-
+        time.sleep(2)  # Várakozás a következő ablak megjelenésére
 
         target_title = "EMP(1)/102 Bekötés megjelenítése:"
+        window1 = GetWindow(target_title)
 
-        
-    window1 = GetWindow(target_title)
+        if window1:
+            print(f"Célablak megtalálva: {window1.window_text()}")
+            window1.set_focus()
+            time.sleep(2)  # Várakozás
 
-    if window1:
+            send_keys('^a')  # Ctrl+a
+            print("'Bekötés' - KIJELÖLVE!")
+            time.sleep(2)
+            send_keys('^c')  # Ctrl+c
+            print("'Bekötés' - KIMÁSOLVA!")
+            time.sleep(1)
+            SendCmd(9, '{TAB}', 1)
+            send_keys('{ENTER}')
+            print("'Időszeletek' - gomb megtalálva!")
+            time.sleep(1)
+            SendCmd(10, '{TAB}')
+            print("'ÜK. Szeg.' - mező megtalálva!")
+            send_keys('^y')
+            send_keys('^c')
+            print("'ÜK. Szeg.' - mező KIMÁSOLVA!")
+        else:
+            target_title = "EMP(1)/102 Információ"
+            window = GetWindow(target_title)
 
-        print(f"Célablak megtalálva: {window1.window_text()}")
-        window1.set_focus()
-        time.sleep(2)  # vár
+            if window:
+                print(f"Célablak megtalálva: {window.window_text()}")
+                window.set_focus()
+                time.sleep(2)  # Várakozás
 
-        send_keys('^a')  #  Ctrl+a
-        print("'Bekötés' - KIJELÖLVE!")
-        time.sleep(2)
-        send_keys('^c')  # Ctrl+c
-        print("'Bekötés' - KIMÁSOLVA!")
-        time.sleep(1)
-        SendCmd(9,'{TAB}',1)
+                print("Nem található a célablak")
+
+                window.set_focus()
+                time.sleep(2)  # Várakozás
+
+                print("Nem található a célablak")
+                time.sleep(1)  # Várakozás
+            else:
+                print("Nem található a célablak ")
+                time.sleep(1)  # Várakozás a célablakra
+
+        print("Folyamat befejezve")
+
+        if window:
+            window.close()
+
+        if window1:
+            window1.set_focus()
+
         send_keys('{ENTER}')
-        print("'Időszeletek' - gomb megtalálva!")
+        SendCmd(21, '{TAB}')
+        send_keys('{ENTER}')
+        time.sleep(1)  # Várakozás a célablakra
+
+def main():
+    # SAP Logon indítása
+    app = Application(backend="win32").start(r"C:\Program Files (x86)\SAP\FrontEnd\SAPgui\saplogon.exe")
+    print("SAP Logon elindult várunk a keresett ablak megnyitására....")
+
+    # Várunk, amíg megjelenik az ablak
+    target_title = "EMP(1)/102 Bekötés megjelenítése: kezdő kép"
+    window = None
+    while not window:
+        window = GetWindow(target_title)
         time.sleep(1)
-        SendCmd(10,'{TAB}')
-        print("'ÜK. Szeg.' - mező megtalálva!")
-        send_keys('^y')
-        send_keys('^c')
-        print("'ÜK. Szeg.' - mező KIMÁSOLVA!")
-
-
-    else:
-
-        target_title = "EMP(1)/102 Információ"
-
-
-    window = GetWindow(target_title)
 
     if window:
-
         print(f"Célablak megtalálva: {window.window_text()}")
         window.set_focus()
-        time.sleep(2)  # vár
+        time.sleep(2)  # Várakozás a célablakra
 
-        print("Nem található a célablak")
-
-        window.set_focus()
-        time.sleep(2)  # vár
-
-
-        print("Nem található a célablak")
-        time.sleep(1)  # vár
-
+        identify_controls(window)
     else:
-        print("Target window not found.")
-        time.sleep(1)  # Observe the target window
+        print("Célablak nem található.")
+        return
 
-    print("Folyamat befejezve")
+    # POD-ok kiolvasása és a folyamat lefuttatása
+    file_path = "C:\\Users\\G3909\\Desktop\\forras.txt"
+    try:
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                Run(line.strip())
+                time.sleep(20)
+    except Exception as e:
+        print(f"Nem olvasható a forrásfájl: {e}")
 
-    time.sleep(1)
-
-    window.close()
-
-    time.sleep(1)
-
-    window1.set_focus()
-
-
-    time.sleep(1)
-    send_keys('{ENTER}')
-    SendCmd(21,'{TAB}')
-    
-
-    send_keys('{ENTER}')
-
-    time.sleep(1)  # Observe the target window
-
-
-file = open("C:\\Users\\G3909\\Desktop\\forras.txt", "r")
-
-lines = file.readlines()
-
-for line in lines:
-    Run(line.replace("\r","").replace("\n",""))
-    time.sleep(200)
+if __name__ == "__main__":
+    main()
